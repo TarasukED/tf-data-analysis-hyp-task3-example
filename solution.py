ad2e7bbb91755b4a6245643beef7a2b603cf4fb1
TarasukED <160397770+TarasukED@users.noreply.github.com>
2024-05-03 21:30:15 +0300

    solution.py

--- solution.py
+++ solution.py
@@ -1,11 +1,10 @@
 import pandas as pd
 import numpy as np
+from scipy.stats import ttest_ind
 
+chat_id = 1742143281 # Ваш chat ID, не меняйте название переменной
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
-
-def solution(...) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+def solution(control_transactions, test_transactions) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
+    stat, p_value = ttest_ind(test_transactions, control_transactions, alternative='greater')
+    alpha = 0.09
+    return p_value < alpha 
